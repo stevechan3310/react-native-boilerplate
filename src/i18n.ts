2855e0e6fe647a1566673d1d444d3d781b3e1c77
Cody <yuchuen90@hotmail.com>
2019-12-11 18:06:29 +0800

    Add iOS configuration
    add iOS configuration of React Native Navigation
    fix Expo Localisation issue
    remove expo localisation package

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,13 @@
-import * as Localization from 'expo-localization';
 import i18n from 'i18next';
+import { NativeModules, Platform } from 'react-native';
 
 import * as enUs from '@/locales/en-US';
 
-const locale = Localization.locale;
+const locale =
+  Platform.OS === 'ios'
+    ? NativeModules.SettingsManager.settings.AppleLocale ||
+      NativeModules.SettingsManager.settings.AppleLanguages[0] //iOS 13
+    : NativeModules.I18nManager.localeIdentifier;
 
 i18n.init({
   fallbackLng: 'en-US',
